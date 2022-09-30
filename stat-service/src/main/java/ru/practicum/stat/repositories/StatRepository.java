801b07d5e7b31cac4c9653131d02379b2770b33a
Denis Gorshkov <gorshkov.dv94@yandex.ru>
2022-10-01 00:21:09 +0400

    Убрать неиспользуемые импорты.

--- stat-service/src/main/java/ru/practicum/stat/repositories/StatRepository.java
+++ stat-service/src/main/java/ru/practicum/stat/repositories/StatRepository.java
@@ -1,12 +1,8 @@
 package ru.practicum.stat.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.stat.models.EndPointHit;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface StatRepository extends JpaRepository<EndPointHit, Integer>, StatCustomRepository {
