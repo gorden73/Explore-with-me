e414736530364468ec0ab6e023e55f71df77b644
Denis Gorshkov <gorshkov.dv94@yandex.ru>
2022-10-01 07:44:17 +0400

    Добавить JavaDoc в основной сервис.

--- main-service/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
+++ main-service/src/main/java/ru/practicum/ewm/ExploreWithMeMain.java
@@ -3,8 +3,20 @@ package ru.practicum.ewm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс микросервиса ExploreWithMeMain
+ *
+ * @since 1.0
+ */
 @SpringBootApplication
 public class ExploreWithMeMain {
+
+    /**
+     * Главный метод микросервиса, отвечающий за его запуск
+     *
+     * @param args передаваемые аргументы
+     * @since 1.0
+     */
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMeMain.class, args);
     }
--- main-service/src/main/java/ru/practicum/ewm/services/CompilationService.java
+++ main-service/src/main/java/ru/practicum/ewm/services/CompilationService.java
@@ -1,27 +1,96 @@
 package ru.practicum.ewm.services;
 
+import ru.practicum.ewm.apis.admins.dtos.compilations.NewCompilationDto;
 import ru.practicum.ewm.models.Compilation;
 import ru.practicum.ewm.models.dtos.compilations.CompilationDto;
-import ru.practicum.ewm.apis.admins.dtos.compilations.NewCompilationDto;
 
 import java.util.Collection;
 
+/**
+ * Интерфейс сервиса для работы с подборками событий
+ *
+ * @see Compilation
+ * @see CompilationDto
+ * @since 1.0
+ */
 public interface CompilationService {
+    /**
+     * Метод позволяет получить коллекцию Dto подборок событий, подходящих под заданные условия
+     *
+     * @param pinned закреплена ли подборка на главной странце
+     * @param from   количество подборок, которое надо пропустить для формирования коллекции
+     * @param size   количество подборок в коллекции
+     * @return коллекция Dto подборок событий
+     * @since 1.0
+     */
     Collection<CompilationDto> getAllCompilations(Boolean pinned, int from, int size);
 
+    /**
+     * Метод позволяет получить Dto подборки событий по идентификатору
+     *
+     * @param id идентификатор подборки
+     * @return Dto подборки событий
+     * @since 1.0
+     */
     CompilationDto getCompilationDtoById(int id);
 
+    /**
+     * Метод позволяет получить подборку событий по идентификатору
+     *
+     * @param id идентификатор подборки
+     * @return подборка событий
+     * @since 1.0
+     */
     Compilation getCompilationById(int id);
 
+    /**
+     * Метод позволяет создать новую подборку событий
+     *
+     * @param compilationDto объект, описывающий основые свойства подборки событий, которые задает администратор
+     * @return созданный объект, описывающий основные и дополнительные свойства подборки событий
+     * @since 1.0
+     */
     CompilationDto addCompilation(NewCompilationDto compilationDto);
 
+    /**
+     * Метод позволяет удалить имеющуюся подборку событий по идентификатору
+     *
+     * @param id идентификатор подборки событий
+     * @since 1.0
+     */
     void removeCompilationById(int id);
 
+    /**
+     * Метод позволяет удалить событие из подборки по идентификаторам подборки и события
+     *
+     * @param compId  идентификатор подборки событий
+     * @param eventId идентификатор события
+     * @since 1.0
+     */
     void removeEventFromCompilation(int compId, int eventId);
 
+    /**
+     * Метод позволяет добавить событие в подборку по идентификаторам подборки и события
+     *
+     * @param compId  идентификатор подборки событий
+     * @param eventId идентификатор события
+     * @since 1.0
+     */
     void addEventToCompilation(int compId, int eventId);
 
+    /**
+     * Метод позволяет открепить подборку по идентификатору от главной страницы
+     *
+     * @param compId идентификатор подборки событий
+     * @since 1.0
+     */
     void unpinCompilationAtMainPage(int compId);
 
+    /**
+     * Метод позволяет закрепить подборку по идентификатору на главной странице
+     *
+     * @param compId идентификатор подборки событий
+     * @since 1.0
+     */
     void pinCompilationAtMainPage(int compId);
 }
